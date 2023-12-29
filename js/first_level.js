8a564f608a6b8eec14ffebbaf6033532a277f563
NoelSerraUbeda <neweonj@gmail.com>
2023-12-29 08:50:08 +0100

    Obstáculos aletorios, iconos rocas y bloque amarillo

--- js/first_level.js
+++ js/first_level.js
@@ -12,105 +12,110 @@ class Level extends HTMLElement {
     }
 
     render() {
-        this.shadow.innerHTML =
-        /*html*/`
+        this.shadow.innerHTML = /*html*/`
+            <style>
+                .grid-container {
+                    background-color: black;
+                    display: grid;
+                    grid-template-columns: repeat(10, 60px);
+                    grid-template-rows: repeat(10, 60px);
+                    gap: 3px;
+                    border: black 20px solid;
+                    border-radius: 1rem;
+                    padding: 0.2rem;
+                }
 
-        <style>
-            .grid-container {
-                background-color:black;
-                display: grid;
-                grid-template-columns: repeat(10, 60px);
-                grid-template-rows: repeat(10, 60px);
-                gap: 3px;
-                border: black 20px solid;
-                border-radius: 1rem;
-                padding: 0.2rem;
-            }
+                .square {
+                    width: 100%;
+                    height: 100%;
+                    background-color: #fff;
+                    box-sizing: border-box;
+                }
 
-            .square {
-                width: 100%;
-                height: 100%;
-                background-color: #fff;
-                box-sizing: border-box;
-            }
+                .start {
+                    grid-row: 10;
+                    grid-column: 1;
+                    background-color: black;
+                    background-image: url(icons/start.svg);
+                    border-radius: 0.5rem;
+                }
 
-            .start {
-                grid-row: 10;
-                grid-column: 1;
-                background-color: black;
-                background-image: url(icons/start.svg);
-                border-radius: 0.5rem;
-            }
+                .end {
+                    grid-row: 1;
+                    grid-column: 10;
+                    background-color: black;
+                    background-image: url(icons/end.svg);
+                    border-radius: 0.5rem;
+                }
 
-            .end {
-                grid-row: 1;
-                grid-column: 10;
-                background-color: black;
-                background-image: url(icons/end.svg);
-                border-radius: 0.5rem;
-            }
+                .path {
+                    background-color: green;
+                }
 
-            .path {
-                background-color: green;
-            }
+                .obstacle {
+                    background-color: brown;
+                    pointer-events: none;
+                    background-image: url(icons/rock.svg);
+                    background-size:cover;
+                }
 
-            .obstacle {
-                background-color: brown;
-                pointer-events: none;
-            }
+                .yellow-block {
+                    background-color: yellow;
+                    background-image: url(icons/plus_5.svg);
+                    background-size:cover;
+                }
 
-            #attempts {
-                position: absolute;
-                top: 50px;
-                left: 50px;
-                font-size: 10rem;
-            }
-
-            .attempts_counter {
-                background-color: lightgrey;
-                padding: 2rem;
-                border: black solid 3px;
-                border-radius: 2rem;
-                width:10rem;
-                height:8rem;
-                display:flex;
-                align-items:center;
-                justify-content:center;
-            }
-
-            .custom-message {
-                position: absolute;
-                top: 10%;
-                left: 50%;
-                transform: translate(-50%, -50%);
-                background-color: #333;
-                color: #fff;
-                padding: 2rem;
-                border-radius: 1.5rem;
-                font-size: 5rem;
-                z-index: 1000;
-                display:none;
-                text-align:center;
-            }
-            @media only screen and (min-width: 600px) {
-                
                 #attempts {
-                    font-size: 5rem;
+                    position: absolute;
+                    top: 50px;
+                    left: 50px;
+                    font-size: 10rem;
                 }
 
                 .attempts_counter {
-                    padding: 0.5rem;
+                    background-color: lightgrey;
+                    padding: 2rem;
+                    border: black solid 3px;
+                    border-radius: 2rem;
+                    width: 10rem;
+                    height: 8rem;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
                 }
 
                 .custom-message {
-                    font-size: 2.5rem;
+                    position: absolute;
+                    top: 10%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                    background-color: #333;
+                    color: #fff;
+                    padding: 2rem;
+                    border-radius: 1.5rem;
+                    font-size: 5rem;
+                    z-index: 1000;
+                    display: none;
+                    text-align: center;
                 }
-            }
 
-        </style>
-        <div class="custom-message"></div>
-        <div class="grid-container" id="gridContainer"></div>
-        <div class="attempts_counter" id="attempts">${this.attempts}</div>
+                @media only screen and (min-width: 600px) {
+                    #attempts {
+                        font-size: 5rem;
+                    }
+
+                    .attempts_counter {
+                        padding: 0.5rem;
+                    }
+
+                    .custom-message {
+                        font-size: 2.5rem;
+                    }
+                }
+            </style>
+            <div class="custom-message"></div>
+            <div class="grid-container" id="gridContainer"></div>
+            <div class="attempts_counter" id="attempts">${this.attempts}</div>
         `;
         this.initializeGrid();
     }
@@ -119,7 +124,15 @@ class Level extends HTMLElement {
         this.shadow.querySelector('#gridContainer').addEventListener('click', (event) => {
             const square = event.target;
             if (this.canMakeMove(square)) {
-                square.classList.add('path');
+                if (square.classList.contains('yellow-block')) {
+                    // Si es un bloque amarillo, cambiar a verde y sumar +5 a los intentos
+                    square.classList.remove('yellow-block');
+                    square.classList.add('path');
+                    this.attempts += 5;
+                } else {
+                    square.classList.add('path');
+                }
+
                 this.checkConnection(square);
                 this.attempts--;
                 this.updateAttemptsCount();
@@ -137,10 +150,49 @@ class Level extends HTMLElement {
             }
         }
 
+        // Añadir bloques amarillos
+        const yellowBlock = this.addYellowBlock(gridContainer);
+
         const startSquare = gridContainer.querySelector('[data-row="9"][data-col="0"]');
         const endSquare = gridContainer.querySelector('[data-row="0"][data-col="9"]');
         startSquare.classList.add('start');
         endSquare.classList.add('end');
+
+        for (let i = 0; i < 30; i++) {
+            const randomRow = Math.floor(Math.random() * 10);
+            const randomCol = Math.floor(Math.random() * 10);
+            const obstacleSquare = gridContainer.querySelector(`[data-row="${randomRow}"][data-col="${randomCol}"]`);
+
+            // Asegurarse de que el obstáculo no esté en las posiciones prohibidas
+            if (obstacleSquare && !this.isProhibitedPosition(randomRow, randomCol) && obstacleSquare !== yellowBlock) {
+                obstacleSquare.classList.add('obstacle');
+            } else {
+                // Si está en una posición prohibida o es el bloque amarillo, intentar de nuevo
+                i--;
+            }
+        }
+    }
+
+    addYellowBlock(gridContainer) {
+        let randomRow, randomCol;
+        do {
+            randomRow = Math.floor(Math.random() * 10);
+            randomCol = Math.floor(Math.random() * 10);
+        } while (this.isProhibitedPosition(randomRow, randomCol));
+
+        const yellowBlock = gridContainer.querySelector(`[data-row="${randomRow}"][data-col="${randomCol}"]`);
+        yellowBlock.classList.add('yellow-block');
+
+        return yellowBlock;
+    }
+
+    isProhibitedPosition(row, col) {
+        const prohibitedPositions = [
+            [0, 8], [1, 8], [1, 9], [0, 9],
+            [8, 0], [8, 1], [9, 1], [9, 0]
+        ];
+
+        return prohibitedPositions.some(pos => pos[0] === row && pos[1] === col);
     }
 
     createSquare(row, col) {
@@ -155,7 +207,8 @@ class Level extends HTMLElement {
     canMakeMove(square) {
         return !this.connectionEstablished && !square.classList.contains('end') &&
             !square.classList.contains('path') && this.attempts > 0 &&
-            this.getNeighbors(square).some(neighbor => neighbor.classList.contains('path') || neighbor.classList.contains('start'));
+            (square.classList.contains('yellow-block') ||
+                this.getNeighbors(square).some(neighbor => neighbor.classList.contains('path') || neighbor.classList.contains('start')));
     }
 
     getNeighbors(clickedSquare) {
@@ -178,15 +231,14 @@ class Level extends HTMLElement {
         const messageContainer = this.shadow.querySelector('.custom-message');
         messageContainer.textContent = message;
         messageContainer.style.display = 'block';
-    
+
         setTimeout(() => {
             messageContainer.style.display = 'none';
         }, durationInSeconds * 1000);
-    
+
         this.connectionEstablished = true;
         this.shadow.querySelector('#attempts').style.display = 'none';
     }
-    
 
     checkConnection(clickedSquare) {
         const clickedRow = parseInt(clickedSquare.dataset.row, 10);
@@ -201,8 +253,6 @@ class Level extends HTMLElement {
         }
     }
 
-
-
     updateAttemptsCount() {
         this.shadow.querySelector('#attempts').textContent = this.attempts;
     }
@@ -212,32 +262,6 @@ class Level extends HTMLElement {
             this.showCustomMessage('Sin intentos disponibles', 3);
         }
     }
-
-    addObstacle(row, col) {
-        const gridContainer = this.shadow.querySelector('#gridContainer');
-        const obstacleSquare = gridContainer.querySelector(`[data-row="${row}"][data-col="${col}"]`);
-
-        if (obstacleSquare && !obstacleSquare.classList.contains('start') && !obstacleSquare.classList.contains('end') && !obstacleSquare.classList.contains('obstacle')) {
-            obstacleSquare.classList.add('obstacle');
-        }
-    }
 }
 
 customElements.define('level-component', Level);
-
-// Coordenadas obstáculos
-const levelComponent = document.querySelector('level-component');
-levelComponent.addObstacle(9, 5);
-levelComponent.addObstacle(9, 4);
-levelComponent.addObstacle(8, 5);
-levelComponent.addObstacle(8, 4);
-levelComponent.addObstacle(7, 5);
-levelComponent.addObstacle(7, 4);
-levelComponent.addObstacle(6, 5);
-levelComponent.addObstacle(6, 4);
-levelComponent.addObstacle(2, 5);
-levelComponent.addObstacle(2, 4);
-levelComponent.addObstacle(1, 5);
-levelComponent.addObstacle(1, 4);
-levelComponent.addObstacle(0, 5);
-levelComponent.addObstacle(0, 4); 
